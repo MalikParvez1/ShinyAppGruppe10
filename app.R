ed994c5e5891b83f229edd675a354ac49548e7d4
MalikParvez1 <97103782+MalikParvez1@users.noreply.github.com>
2022-12-30 16:15:28 +0100

    app.R

--- app.R
+++ app.R
@@ -1,3 +1,42 @@
-install.packages("shiny")
 library(shiny)
-library(shinythemes)
+library(shinydashboard)
+
+ui <- fluidPage(
+  
+  navbarPage("Covid",
+             tabPanel("Startseite",
+                      sidebarLayout(
+                        sidebarPanel(
+                          radioButtons("plotType", "Plot type",
+                                       c("Scatter"="p", "Line"="l")
+                          )
+                        ),
+                        mainPanel(
+                          plotOutput("plot")
+                        )
+                      )
+             ),
+             tabPanel("Bezirke",
+                      sidebarPanel(
+                        helpText("Hier können Sie den verlauf der Covidinfektion in den einzelnen Bezirken verfolgen."),
+                        
+                        selectInput("var", 
+                                    label = "Wählen Sie einen Bezirk aus",
+                                    choices = list("Wedding", 
+                                                   "Neukölln"),
+                                    selected = "Percent White"),
+                        
+                        sliderInput("range", 
+                                    label = "Range of interest:",
+                                    min = 0, max = 100, value = c(0, 100))
+                      )
+             )
+  )
+
+)
+
+server <- function(input, output) {
+
+}
+
+shinyApp(ui = ui, server = server)
